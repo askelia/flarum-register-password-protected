1aa1a69a22f2a954824a7bdfcfbc1a0b14a6e226
Emmanuel Bruno <e.bruno@askelia.com>
2021-11-14 09:30:47 +0000

    restored

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,11 +1,11 @@
-import {SignUpModal} from 'flarum/forum/components/HeaderPrimary.js'; //SignUpModal.js';
 import { extend } from 'flarum/common/extend';
+import SignUpModal from 'flarum/forum/components/SignUpModal.js';
 
-
+/*
 app.initializers.add('askelia/flarum-register-password-protected', () => {
   console.log('[askelia/flarum-register-password-protected] Hello, forum!');
 });
-
+*/
 
 	extend(SignUpModal.prototype, 'items', function(items){
 		items.add('key', <div>Key<input type="password"/></div>);
