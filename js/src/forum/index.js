7c5f98d70f26d92dc41a4af2f170cd3bd2c03c46
Emmanuel Bruno <e.bruno@askelia.com>
2021-11-14 09:50:07 +0000

    put extend into initializers back

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,11 @@
 import { extend } from 'flarum/common/extend';
 import SignUpModal from 'flarum/forum/components/SignUpModal.js';
 
-/*
-app.initializers.add('askelia/flarum-register-password-protected', () => {
-  console.log('[askelia/flarum-register-password-protected] Hello, forum!');
-});
-*/
 
+app.initializers.add('askelia/flarum-register-password-protected', () => {
+  	console.log('[askelia/flarum-register-password-protected] Hello, forum!');
 	extend(SignUpModal.prototype, 'items', function(items){
 		items.add('key', <div>Key<input type="password"/></div>);
 	});
 
+});
